84802cb1b582e502d1d630a051b8954771e2ba86
ding <397737198@qq.com>
2018-12-04 10:44:21 +0800

    前端代码

--- src/routes/workManager/workEvents/WorkEvent.js
+++ src/routes/workManager/workEvents/WorkEvent.js
@@ -21,7 +21,7 @@ const Modal = ModalForm.Modal;
 const confirm = Modal.confirm;
 const Search = Input.Search;
 
-const data = [
+const dataTest = [
   {
     id:1,
     title: '合肥管廊运营处_紧急巡视',
@@ -34,7 +34,8 @@ const data = [
     gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
     ops:'系统管理员',
     eventContent:'彩虹西路管廊发生烟雾报警，彩虹西路管廊发生烟雾报警',
-    workLevel:'一级'
+    workLevel:'一级',
+    status:'进行中'
   },
   {
     id:2,
@@ -48,7 +49,8 @@ const data = [
     gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
     ops:'系统管理员',
     eventContent:'彩虹西路管廊发生烟雾报警',
-    workLevel:'一级'
+    workLevel:'一级',
+    status:'进行中'
   },
   {
     id:3,
@@ -62,7 +64,8 @@ const data = [
     gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
     ops:'系统管理员',
     eventContent:'彩虹西路管廊发生烟雾报警',
-    workLevel:'一级'
+    workLevel:'一级',
+    status:'等待中'
   },
   {
     id:4,
@@ -76,65 +79,11 @@ const data = [
     gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
     ops:'系统管理员',
     eventContent:'彩虹西路管廊发生烟雾报警',
-    workLevel:'一级'
+    workLevel:'一级',
+    status:'等待中'
   },
 ];
-const columns = [
-  {
-    title: '用户名',
-    dataIndex: 'userName',
-    id: 'userName',
-    align: 'center',
-    key:'userName'
-  }, {
-    title: '登录名',
-    dataIndex: 'loginName',
-    id: 'loginName',
-    align: 'center',
-    key:'loginName'
-  },{
-    title: '证件号码',
-    dataIndex: 'card',
-    id: 'card',
-    align: 'center',
-    key:'card'
-  }, {
-    title: '机构名称',
-    dataIndex: 'orgName',
-    id: 'orgName',
-    align: 'center',
-    key:'orgName'
-  },{
-    title: '角色',
-    dataIndex: 'roleName',
-    id: 'roleName',
-    align: 'center',
-    key:'roleName'
-  }, {
-    title: '固定电话',
-    dataIndex: 'phone',
-    id: 'phone',
-    align: 'center',
-    key:'phone'
-  }, {
-    title: '手机',
-    dataIndex: 'mobilePhone',
-    id: 'mobilePhone',
-    align: 'center',
-    key:'mobilePhone'
-  }, {
-    title: '邮箱',
-    dataIndex: 'email',
-    id: 'email',
-    align: 'center',
-    key:'email'
-  }, {
-    title: 'QQ',
-    dataIndex: 'qq',
-    id: 'qq',
-    align: 'center',
-    key:'qq'
-  }];
+
 const Paging = ({dataItems, onChange, ...otherProps}) => {
   const { total, pageSize, pageNum } = dataItems;
   const paging = {
@@ -158,15 +107,13 @@ export default class workEvent extends PureComponent {
   };
 
   state = {
-    columns:[],
     dataSource:[],
     fileDataSource:[],
     record: null,
     visible: false,
     rows: [],
     openSide: true,
-    treeData:[],
-    treeDataSote:[],
+    data:[],
   };
 
   constructor(props,context) {
@@ -178,7 +125,10 @@ export default class workEvent extends PureComponent {
   }
 
   init= () =>{
-    const thiz = this;
+    this.setState({
+      data:dataTest
+    })
+    /*const thiz = this;
     GET('/users',function(data){
       if(data.success){
         thiz.setState({
@@ -190,13 +140,13 @@ export default class workEvent extends PureComponent {
       }
     },function(error){
       console.log(error)
-    })
-  }
+    })*/
+  };
 
   //编辑
   edit =(item)=>{
     this.openModal(item);
-  }
+  };
 
   //新增事件
   onAdd = () => {
@@ -213,20 +163,20 @@ export default class workEvent extends PureComponent {
         width: 700,
       },
       onSubmit: (values) => this.onSubmit(values)
-    }
+    };
     this.context.openModal(modalFormProps);
-  }
+  };
 
   delete =(item)=> {
     const {rows,record} = this.state;
     alert(item.id);
-  }
+  };
 
   //选中项发生变化时的回调
   onSelectChange = (selectedRowKeys,selectedRows) => {
     console.log('selectedRowKeys changed: ', selectedRowKeys);
     this.setState({rows:selectedRows,record:selectedRows[0]});
-  }
+  };
 
   onSubmit= (values) =>{
     const thiz = this;
@@ -259,25 +209,102 @@ export default class workEvent extends PureComponent {
       })
 
     }
-  }
+  };
 
   closeModal = () =>{
     this.setState({
       visible: false
     });
-  }
+  };
 
   onSelect = (selectedKey) => {
 
-  }
+  };
 
   handleSearch = () => {
 
+  };
+
+  filter = (flag) => {
+    if(flag == '进行中'){
+        this.setState({
+          data:[
+            {
+              id:1,
+              title: '合肥管廊运营处_紧急巡视',
+              unitName:'合肥管廊运营管理处',
+              work_user:'周福',
+              reasons:'临时任务',
+              workType:'紧急巡视',
+              startDate:'2018-03-13 08：30：00',
+              endDate:'2018-03-13 17：30：00',
+              gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
+              ops:'系统管理员',
+              eventContent:'彩虹西路管廊发生烟雾报警，彩虹西路管廊发生烟雾报警',
+              workLevel:'一级',
+              status:'进行中'
+            },
+            {
+              id:2,
+              title: '合肥管廊运营处_紧急巡视',
+              unitName:'合肥管廊运营管理处',
+              work_user:'周福',
+              reasons:'临时任务',
+              workType:'紧急巡视',
+              startDate:'2018-03-13 08：30：00',
+              endDate:'2018-03-13 17：30：00',
+              gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
+              ops:'系统管理员',
+              eventContent:'彩虹西路管廊发生烟雾报警',
+              workLevel:'一级',
+              status:'进行中'
+            },
+          ]
+        })
+    }else if(flag == '等待中'){
+      this.setState({
+        data:[
+          {
+            id:3,
+            title: '合肥管廊运营处_紧急巡视',
+            unitName:'合肥管廊运营管理处',
+            work_user:'周福',
+            reasons:'临时任务',
+            workType:'紧急巡视',
+            startDate:'2018-03-13 08：30：00',
+            endDate:'2018-03-13 17：30：00',
+            gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
+            ops:'系统管理员',
+            eventContent:'彩虹西路管廊发生烟雾报警，彩虹西路管廊发生烟雾报警',
+            workLevel:'一级',
+            status:'等待中'
+          },
+          {
+            id:4,
+            title: '合肥管廊运营处_紧急巡视',
+            unitName:'合肥管廊运营管理处',
+            work_user:'周福',
+            reasons:'临时任务',
+            workType:'紧急巡视',
+            startDate:'2018-03-13 08：30：00',
+            endDate:'2018-03-13 17：30：00',
+            gallery_name:'彩虹西路(将军岭路~鸡鸣山路)',
+            ops:'系统管理员',
+            eventContent:'彩虹西路管廊发生烟雾报警',
+            workLevel:'一级',
+            status:'等待中'
+          },
+        ]
+      })
+    }else{
+      this.setState({
+        data:dataTest
+      })
+    }
   }
 
-
   render() {
-    const {visible,record,dataSource,treeData} = this.state;
+    const {data,record,dataSource,treeData} = this.state;
     const rowSelection = {
       onChange: this.onSelectChange,
     };
@@ -285,9 +312,9 @@ export default class workEvent extends PureComponent {
     const extraContent = (
       <div>
         <RadioGroup defaultValue="all">
-          <RadioButton value="all">全部</RadioButton>
-          <RadioButton value="progress">进行中</RadioButton>
-          <RadioButton value="waiting">等待中</RadioButton>
+          <RadioButton value="all" onClick={()=>this.filter('全部')}>全部</RadioButton>
+          <RadioButton value="progress" onClick={()=>this.filter('进行中')}>进行中</RadioButton>
+          <RadioButton value="waiting" onClick={()=>this.filter('等待中')}>等待中</RadioButton>
         </RadioGroup>
         <Search style={{marginLeft: 16,width: 272}} placeholder="请输入" onSearch={() => ({})} />
       </div>
@@ -295,7 +322,7 @@ export default class workEvent extends PureComponent {
 
     const tittleContent = (
         <ButtonAuthorize icon="plus" type="primary" onClick={this.onAdd} name="新增" authority="role:add" style={{margin:'0px'}}/>
-    )
+    );
 
     return(
       <div>
@@ -362,7 +389,7 @@ export default class workEvent extends PureComponent {
               renderItem={item =>
                 (
                   <List.Item key={item.id}>
-                    <Card hoverable  title={<h5>{item.title}<span style={{marginLeft:'32%' , color:'#1890ff'}}>进行中</span></h5>}
+                    <Card hoverable  title={<h5>{item.title}<span style={{marginLeft:'32%' , color:'#1890ff'}} >{item.status}</span></h5>}
                           actions={[<Icon type="edit" onClick={() => this.edit(item)}/>, <Icon type="close" onClick={() => this.delete(item)}/>]}
                           style={{background: '#f3f3f3'}}>
                       <p style={{fontSize:14 ,color:'#9674ce'}}>{item.eventContent}</p>

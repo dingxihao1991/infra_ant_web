9de6483cc077e26a29bc2c1f9e16cb99c012fe98
Zhangyl <zhangyl769405468@gmail.com>
2019-04-09 10:35:17 +0800

    模型页面方式修改

---
+++ src/routes/model/Decorate.js
@@ -0,0 +1,30 @@
+import React, {PureComponent,Component} from 'react';
+import { connect } from 'dva';
+import PropTypes from 'prop-types';
+import { Table ,Button ,Layout,List} from 'antd';
+import {ModalForm,showConfirm}  from 'components/Modal';
+import SideLayout from 'components/SideLayout';
+import styles from './index.less';
+const { Content} = Layout;
+
+
+
+class Decorate extends Component {
+
+
+    constructor(props,context) {
+        super(props,context);
+    }
+
+
+
+
+
+    render() {
+        return(
+            <div></div>
+        )
+
+    }
+}
+export default Decorate

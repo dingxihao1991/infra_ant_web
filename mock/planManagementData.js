dc9a6bcd9c534758272f008bb98e55f7e27bfc3d
Zhangyl <zhangyl769405468@gmail.com>
2019-04-25 14:53:32 +0800

    里边文字超出样式更改为全局

--- mock/planManagementData.js
+++ mock/planManagementData.js
@@ -1,59 +1,71 @@
-const list = [{
-  'id':'e1',
-  1: '1',
-  2: '安检预案',
-  3: '常规预案',
-  4: '综合预案',
-  5: 'YA0000001',
-  6: '复兴东路隧道系统—安防部',
-  7: '已备案',
-  8: '3',
-  9: '2018-12-17',
-}, {
-  'id':'e2',
-  1: '1',
-  2: '消防预案',
-  3: '常规预案',
-  4: '综合预案',
-  5: 'YA0000002',
-  6: '上中路隧道系统—安防部',
-  7: '已备案',
-  8: '3',
-  9: '2018-12-17',
-}, {
-  'id':'e3',
-  1: '1',
-  2: '地震预案',
-  3: '常规预案',
-  4: '综合预案',
-  5: 'YA0000003',
-  6: '上中路隧道系统—安防部',
-  7: '已备案',
-  8: '3',
-  9: '2018-12-17',
-}, {
-  'id':'e4',
-  1: '1',
-  2: '重大事故预案',
-  3: '常规预案',
-  4: '综合预案',
-  5: 'YA0000004',
-  6: '西藏南路隧道系统—安防部',
-  7: '已备案',
-  8: '3',
-  9: '2018-12-17',
-}, {
-  'id':'e5',
-  1: '1',
-  2: '质量预案',
-  3: '常规预案',
-  4: '综合预案',
-  5: 'YA0000005',
-  6: '西藏南路隧道系统—质量部',
-  7: '已备案',
-  8: '3',
-  9: '2018-12-17',
-}];
+const list = [
+    {
+        id: 'y100001',
+        mold: "消防",
+        daycount: 10,
+        sysdate: new Date(),
+        tid: "1",
+        templateName: "火灾应急预案",
+        tdescription: "火灾应急",
+        grade: "严重（级）",
+        putOnRecords:'是'
+    },
+    {
+        id: 'y100002',
+        mold: "暴雨",
+        daycount: 1,
+        sysdate: new Date(),
+        tid: "402881fe62dc64c50162dce1e42a0001",
+        templateName: "暴雨灾害预警（1）",
+        tdescription: "12小时降雨量将达50毫米以上，或者已达50毫米以上且降雨可能持续。",
+        grade: "一般（级）",
+        putOnRecords:'是'
+    },
+    {
+        id: 'y100003',
+        mold: "电力",
+        daycount: 6,
+        sysdate: new Date(),
+        tid: "4028e4f6630ab2c601630ab7a8180001",
+        templateName: "测试预案",
+        tdescription: "测试预案",
+        grade: "较重（级）",
+        putOnRecords:'是'
+    },
+    {
+        id: 'y100004',
+        mold: "暴雨",
+        daycount: 2,
+        sysdate: new Date(),
+        tid: "402881fe62d77b580162d77f4bf50000",
+        templateName: "暴雨灾害预警（2）",
+        tdescription: "6小时降雨量将达50毫米以上，或者已达50毫米以上且降雨可能持续",
+        grade: "较重（级）",
+        putOnRecords:'是'
+    },
+    {
+        id: 'y100005',
+        mold: "其它",
+        daycount: 3,
+        sysdate: new Date(),
+        tid: "402881fe62dd077e0162dd194fc20001",
+        templateName: "水管爆裂",
+        tdescription: "水管爆裂应急预案",
+        grade: "较重（级）",
+        putOnRecords:'是'
+    },
+    {
+        id: 'y100006',
+        mold: "电力",
+        daycount: 2,
+        sysdate: new Date(),
+        tid: "402881fe631f663601631f8969960004",
+        templateName: "测试一",
+        tdescription: "灰化肥会发挥",
+        grade: "一般（级）",
+        putOnRecords:'是'
+    }
+]
 
 export default {
   // 支持值为 Object 和 Array

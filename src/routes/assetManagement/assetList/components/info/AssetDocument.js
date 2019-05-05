8d6af4a07a4d88d2b2225d53ba36b043ff148ac8
769405479@qq.com <wuyanli199482@>
2019-05-05 14:56:37 +0800

    修改数据

--- src/routes/assetManagement/assetList/components/info/AssetDocument.js
+++ src/routes/assetManagement/assetList/components/info/AssetDocument.js
@@ -9,7 +9,7 @@ export default class AssetDocument extends PureComponent{
 
         return(
             <Layout style={{height:'100%'}}>
-                <img src='/images/bg2.png' width="100%" height="100%"/>
+                <img src='/static/public/images/bg2.png' width="100%" height="100%"/>
             </Layout>
         )
     }
